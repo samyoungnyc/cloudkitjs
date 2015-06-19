8ac8052c0e1c438efadd6c553dc1bc2fb92e1bd8
samyoungnyc <sambyoung@gmail.com>
2015-06-19 18:23:37 -0400

    moved js query from inline HTML to query.js file

--- js/cloudkit-code-samples/public-query.js
+++ js/cloudkit-code-samples/public-query.js
@@ -1,136 +1,61 @@
-/*
-Copyright (C) 2015 Apple Inc. All Rights Reserved.
-See LICENSE.txt for this sample’s licensing information
+function readQuery() {
 
-Abstract:
-Sample code for performing a location query for Items objects in the public database. Includes rendering helpers.
-*/
-
-CKCatalog.tabs['public-query'] = (function() {
-
-  var renderItem = function (name, location, assetUrl) {
-    var item = document.createElement('div');
-    item.className = 'item';
-
-    // Div to clear floats.
-    var clear = document.createElement('div');
-    clear.className = 'clear';
-
-    // Build the img element.
-    var imageEl = document.createElement('img');
-    imageEl.src = assetUrl;
-    imageEl.setAttribute('width', '300');
-    imageEl.className = 'item-asset';
-
-    // Build the name element.
-    var nameEl = document.createElement('h1');
-    nameEl.className = 'item-name';
-    nameEl.textContent = name;
-
-    // Build the location element.
-    var locationEl = document.createElement('div');
-    locationEl.className = 'item-location';
-
-    var latitude = document.createElement('div');
-    var latitudeTitle = document.createElement('span');
-    latitudeTitle.className = 'item-location-title';
-    latitudeTitle.textContent = 'Latitude: ';
-    latitude.appendChild(latitudeTitle);
-    latitude.appendChild(document.createTextNode(location.latitude));
-
-    var longitude = document.createElement('div');
-    var longitudeTitle = document.createElement('span');
-    longitudeTitle.className = 'item-location-title';
-    longitudeTitle.textContent = 'Longitude: ';
-    longitude.appendChild(longitudeTitle);
-    longitude.appendChild(document.createTextNode(location.longitude));
-
-    locationEl.appendChild(latitude);
-    locationEl.appendChild(longitude);
-
-    // Append all children.
-    item.appendChild(imageEl);
-    item.appendChild(nameEl);
-    item.appendChild(locationEl);
-    item.appendChild(clear);
-
-    return item;
-  };
-
-  var render = function(title) {
-    var content = document.createElement('div');
-    var heading = document.createElement('h2');
-    heading.textContent = title;
-    content.appendChild(heading);
-    return content;
-  };
-
-  var getUsersPosition = function() {
-    return new CloudKit.Promise(function(resolve,reject) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var coordinates = position.coords;
-        resolve({
-          latitude: coordinates.latitude,
-          longitude: coordinates.longitude
-        });
-      },reject);
-    });
-  };
-
-  var publicQuerySample = {
-    title: 'performQuery',
-    sampleCode: function demoPerformQuery() {
-      var container = CloudKit.getDefaultContainer();
-      var publicDB = container.publicCloudDatabase;
-
-      // Get the user's current geolocation.
-      return getUsersPosition().then(function (position) {
-
-        // position is an object containing keys 'latitude' and 'longitude'.
+        var container = CloudKit.getDefaultContainer();
+        var publicDB = container.publicCloudDatabase;
 
         // Set up a query that sorts results in ascending distance from the
-        // user's location.
+        // above location.
         var query = {
-          recordType: 'Items',
-          sortBy: [{
-            fieldName: 'location',
-            relativeLocation: position
-          }]
+            recordType: 'Locations'
         };
 
         // Execute the query.
         return publicDB.performQuery(query)
-          .then(function (response) {
-            // if(response.hasErrors) {
+            .then(function(response) {
+                if (response.hasErrors) {
+                    // Handle them in your app.
+                    throw response.errors[0];
+                } else {
+                    var records = response.records;
+                    var numberOfRecords = records.length;
 
-            //   // Handle them in your app.
-            //   throw response.errors[0];
+                    if (numberOfRecords === 0) {
+                        return render('No matching locations')
+                    } else {
+                        var startOfTable = "<table border=1>"
+                        document.write(startOfTable);
 
-            // } else {
-            //   var records = response.records;
-            //   var numberOfRecords = records.length;
-            //   if (numberOfRecords === 0) {
-            //     return render('No matching items')
-            //   } else {
-            //     var el = render('Found ' + numberOfRecords + ' matching item'
-            //       + (numberOfRecords > 1 ? 's' : ''));
-            //     records.forEach(function (record) {
-            //       var fields = record.fields;
-            //       el.appendChild(renderItem(
-            //         fields['name'].value,
-            //         fields['location'].value,
-            //         fields['asset'].value.downloadURL
-            //       ));
-            //     });
-            //     return el;
-            alert("ran the query")
-              }
-            }
-          })
-      });
+                        var tableHead = "<tr><th style='width: 100px'>Place Name</th>";
+                        tableHead += "<th>Category</th>";
+                        tableHead += "<th>Address</th>";
+                        tableHead += "<th>City</th>";
+                        tableHead += "<th>State</th>";
+                        tableHead += "<th style='width: 100px'>Zip Code</th>";
+                        tableHead += "<th>Country</th>";
+                        tableHead += "<th>Days and Hours</th>";
+                        tableHead += "<th>Contact</th>";
+                        tableHead += "<th>Phone</th>";
+                        tableHead += "<th>Email</th>";
+                        tableHead += "<th>Site</th>";
+                        tableHead += "<th>Facebook</th>";
+                        tableHead += "<th>Twitter</th>";
+                        tableHead += "<th>Instagram</th>";
+                        tableHead += "<th>Latitude</th>";
+                        tableHead += "<th>Longitude</th>";
+                        tableHead += "<th>Notes</th></tr>";
+
+                        document.write(tableHead);
+
+                        records.forEach(function(record) {
+                            var fields = record.fields;
+                            var tableActual = "<tr><td>" + fields['placeName'].value + "</td><td>" + fields['category'].value + "</td><td>" + fields['address'].value + "</td><td>" + fields['city'].value + "</td><td>" + fields['state'].value + "</td><td>" + fields['zipCode'].value + "</td><td>" + fields['country'].value + "</td><td>" + fields['daysHours'].value + "</td><td>" + fields['contact'].value + "</td><td>" + fields['phone'].value + "</td><td>" + fields['email'].value + "</td><td>" + fields['site'].value + "</td><td>" + fields['facebook'].value + "</td><td>" + fields['twitter'].value + "</td><td>" + fields['instagram'].value + "</td><td>" + fields['location'].value + "</td><td>" + fields['location'].value + "</td><td>" + fields['notes'].value + "</td></tr>"
+
+                            document.write(tableActual)
+                        });
+                        var endOfTable = "</table>"
+                        document.write(endOfTable)
+
+                    }
+                }
+            });
     }
-  };
-
-  return [ publicQuerySample ];
-
-})();
