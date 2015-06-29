9a9e81bc471fa78df17578bd3f107b9b8cf3b5c1
samyoungnyc <sambyoung@gmail.com>
2015-06-29 18:55:34 -0400

    Create feature implemented, must fix Read feature for new entries

--- js/cloudkit-code-samples/add-record.js
+++ js/cloudkit-code-samples/add-record.js
@@ -1,9 +1,8 @@
 
-function saveRecord(placeName, state, zipCode) {
-	alert("placeName:"+ placeName + "\n" + "state" + state + "\n" + "zip" + zipCode);
+function saveRecord(placeName, category, address, city, state, zipCode, country, daysAndHours, contact, phone, email, site, facebook, twitter, instagram, notes) {
 
 	var container = CloudKit.getDefaultContainer();
-	var privateDB = container.privateCloudDatabase;
+	var publicDB = container.publicCloudDatabase;
 
 	var record = {
 		// recordName: recordName,
@@ -14,17 +13,55 @@ function saveRecord(placeName, state, zipCode) {
 			placeName: {
 				value: placeName
 			},
+			category: {
+				value: category
+			},
+			address: {
+				value: address
+			},
+			city: {
+				value: city
+			},
 			state: {
 				value: state
 			},
 			zipCode: {
 				value: zipCode
+			},
+			country: {
+				value: country
+			},
+			daysAndHours: {
+				value: daysAndHours
+			},
+			contact: {
+				value: contact
+			},
+			phone: {
+				value: phone
+			},
+			email: {
+				value: email
+			},
+			site: {
+				value: site
+			},
+			facebook: {
+				value: facebook
+			},
+			twitter: {
+				value: twitter
+			},
+			instagram: {
+				value: instagram
+			},
+			notes: {
+				value: notes
 			}
 		}
 	};
 
-return privateDB.saveRecord(record).then(function(response) {
-		alert("privateDB")
+return publicDB.saveRecord(record).then(function(response) {
 
 		if (response.hasErrors) {
 			throw response.errors[0];
@@ -32,8 +69,22 @@ return privateDB.saveRecord(record).then(function(response) {
 			var createdRecord = response.records[0];
 			var fields = createdRecord.fields;
 			var placeName = fields['placeName'];
+			var category = fields['category'];
+			var address = fields['address'];
+			var city = fields['city'];
 			var state = fields['state'];
 			var zipCode = fields['zipCode'];
+			var country = fields['country'];
+			var daysAndHours = fields['daysAndHours'];
+			var contact = fields['contact'];
+			var phone = fields['phone'];
+			var email = fields['email'];
+			var site = fields['site'];
+			var facebook = fields['facebook'];
+			var twitter = fields['twitter'];
+			var instagram = fields['instagram'];
+			var notes = fields['notes'];
+
 
 			return createdRecord
 		}
