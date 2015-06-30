8f710691801953d44aa52abbbb15ab2305d03a7b
samyoungnyc <sambyoung@gmail.com>
2015-06-30 15:49:23 -0400

    fixed error with public-query.js

--- js/cloudkit-code-samples/public-query.js
+++ js/cloudkit-code-samples/public-query.js
@@ -20,7 +20,6 @@ function readQuery() {
             } else {
                 var records = response.records;
                 var numberOfRecords = records.length;
-
                 if (numberOfRecords === 0) {
                     return render('No matching locations')
                 } else {
@@ -50,7 +49,6 @@ function readQuery() {
 
                     records.forEach(function(record) {
                         var fields = record.fields;
-                        // var tableActual = "<tr><td>" + record['created'].timestamp + "</td><td>" + fields['placeName'].value + "</td><td>" + fields['category'].value + "</td><td>" + fields['address'].value + "</td><td>" + fields['city'].value + "</td><td>" + fields['state'].value + "</td><td>" + fields['zipCode'].value + "</td><td>" + fields['country'].value + "</td><td>" + fields['daysAndHours'].value + "</td><td>" + fields['contact'].value + "</td><td>" + fields['phone'].value + "</td><td>" + fields['email'].value + "</td><td>" + fields['site'].value + "</td><td>" + fields['facebook'].value + "</td><td>" + fields['twitter'].value + "</td><td>" + fields['instagram'].value + "</td><td>" + fields['location'].value + "</td><td>" + fields['location'].value + "</td><td>" + fields['notes'].value + "</td><td><button>Delete</button></td></tr>"
                         var tableActual = "<tr><td>" + record['created'].timestamp + "</td><td>" + fields['placeName'].value + "</td><td>" + fields['category'].value + "</td><td>" + fields['address'].value + "</td><td>" + fields['city'].value + "</td><td>" + fields['state'].value + "</td><td>" + fields['zipCode'].value + "</td><td>" + fields['country'].value + "</td><td>" + fields['daysAndHours'].value + "</td><td>" + fields['contact'].value + "</td><td>" + fields['phone'].value + "</td><td>" + fields['email'].value + "</td><td>" + fields['site'].value + "</td><td>" + fields['facebook'].value + "</td><td>" + fields['twitter'].value + "</td><td>" + fields['instagram'].value + "</td><td>" + fields['notes'].value + "</td><td><button>Delete</button></td></tr>"
 
                         document.write(tableActual)
